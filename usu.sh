9420859406db480b2a7437cd3987c53aaf32dc35
Isaac <91330447+alurue9140@users.noreply.github.com>
2022-02-11 15:41:39 +0100

    Update usu.sh

--- usu.sh
+++ usu.sh
@@ -1,6 +1,6 @@
- #!/bin/bash
+#!/bin/bash
 
-while IFS=. read -r col1 col2 col3 col4 col5
+while IFS=. read -r col1 col2 col3 col4 col5 col6 col7
 do
     echo "I got:$col1|$col2"
         # Extraemos los campos de los usuarios
@@ -9,10 +9,13 @@ do
         APELLIDOS=$col3
         UO=$col4
         GRUPO=$col5
+        DEP=$col6
+        MAIL=$col7
         #Añadimos el usuario con samba-tool y lo añadimos a la Unidad Organizativa grupo que le corresponde
         echo -n "Añadiendo usuario $LOGIN..."
         #Añade el usuario en la UO correspondiente
-        samba-tool user create $LOGIN zsx100* --given-name=$NOMBRE --surname=$APELLIDOS --must-change-at-next-login --u>        #Se hace miembro del grupo correspondiente al usuario
+        samba-tool user create $LOGIN zsx100* --given-name=$NOMBRE --surname=$APELLIDOS --must-change-at-next-login --userou=$UO --department=$DEP --mail=$MAIL
+        #Se hace miembro del grupo correspondiente al usuario
         samba-tool group addmembers $GRUPO $LOGIN
         echo "[Usuario $LOGIN creado correctamente]"
 
